9cd852395a03f498ca2a7572fa662d396c925ccc
Sandhya-ad <adhikar2@ualberta.ca>
2026-03-15 18:09:56 -0600

    little bit into added

---
+++ src/components/intro/intro.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./intro.css";
+import bg from "../../assets/profilePic.jpeg";
+
+const Intro = () => {
+  return (
+    <section className="intro">
+      <div className="introContent">
+        <span className="hello">Hello</span>
+        <span className="introText">
+          I'm <span className="introName">Sandhya</span>
+          <br />
+          Website Designer
+        </span>
+        <p className="introPara">
+          I'm a 4th year Computer Science student. I love blah blah.
+        </p>
+      </div>
+
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
+};
+
+export default Intro;
