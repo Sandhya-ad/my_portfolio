472f2657d44f91daf40940dbbe5281e2659b5ebd
Sandhya-ad <adhikar2@ualberta.ca>
2026-03-15 21:58:16 -0600

    add a random fact API

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./intro.css";
 import bg from "../../assets/profilePic.jpeg";
 
 const Intro = () => {
+
+    const [fact, setFact] = useState("");
+
+    const fetchRandomFact = async () => {
+        try {
+            const response = await fetch("https://api.api-ninjas.com/v1/facts", {
+                headers:{
+                    "X-API-Key":process.env.REACT_APP_FACTS_API_KEY
+                }
+    });
+            const data = await response.json();
+            setFact(data[0].fact);
+        } catch (error) {
+            setFact("Could not fetch a random fact at this time.");
+        }
+    };
   return (
     <section className="intro">
       <div className="introContent">
@@ -15,6 +31,10 @@ const Intro = () => {
         <p className="introPara">
           I'm a 4th year Computer Science student. I love blah blah.
         </p>
+        <span className ="randomFact">{fact && <>Random fact:{fact}</>}</span>
+        <button className="factButton" onClick={fetchRandomFact}>
+          {fact ? "Show Another Fact" :"Show Random Fact"}
+        </button>
       </div>
 
       <img src={bg} alt="Profile" className="bg" />
